41874cdad9a9324ded4bcb478b30890cbaff9aab
Antonio <oz0969991415@gmail.com>
2023-10-02 11:02:53 +0300

    added all

---
+++ app/src/test/java/task6_dev/ClientServices/ClientServiceTest.java
@@ -0,0 +1,12 @@
+package task6_dev.ClientServices;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientServiceTest {
+
+    @Test
+    void create() {
+    }
+}
